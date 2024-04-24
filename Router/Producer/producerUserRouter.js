ba616b2446a6ed86ed8b748af5ac5f015b271f4e
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-04-24 15:40:29 +0530

    Update producerUserRouter.js

--- Router/Producer/producerUserRouter.js
+++ Router/Producer/producerUserRouter.js
@@ -19,3 +19,36 @@ router.get("/", async (req, res) => {
 
   export const producerRouterUser = router;
 
+ router.post("/", async (req, res) => {
+    try {
+      let producer = await Producer.findOne({name:{$eq:req.body.name}});
+      console.log(producer)
+      if(producer){
+        return  res.status(409).json({message:"Producer  Already Exist"})
+      }
+      producer=await Producer.create(req.body)
+      return res.status(200).json({ message:"Producer added Successfully"});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message:"Internal Server Error"});
+    }
+  });
+
+
+  router.put("/:id", async (req, res) => {
+   
+    try {
+      const updatedProducer= await Producer.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+      );
+      if (!updatedProducer) {
+        return res.status(400).json({ message: "Couldn'nt update your data" });
+      }
+      return res.status(200).json({ message:"updated Successfully"});
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
