e03d69a22eb3c289a799920bfa0dec76a6192b41
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-04-24 15:39:20 +0530

    Update actorUserRouter.js

--- Router/Actor/actorUserRouter.js
+++ Router/Actor/actorUserRouter.js
@@ -19,3 +19,35 @@ router.get("/", async (req, res) => {
 
   export const actorRouterUser = router;
 
+ router.post("/", async (req, res) => {
+    try {
+      let actor = await Actor.findOne({name:req.body.name});;
+      if(actor){
+        return  res.status(409).json({message:"Actor Already Exist"})
+      }
+      actor=await Actor.create(req.body)
+      return res.status(200).json({message:"Actor data added Successfully"});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({message:"Internal Server Error"});
+    }
+  });
+
+
+  router.put("/:id", async (req, res) => {
+    try {
+      const updatedActor= await Actor.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true }
+      );
+      if (!updatedActor) {
+        return res.status(400).json({ message: "Couldn'nt update your content" });
+      }
+      return res.status(200).json({ message:"updated Successfully"});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
